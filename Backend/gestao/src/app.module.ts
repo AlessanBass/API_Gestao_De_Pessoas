6bfa0ebdf5be1c5baa79197cbd7e333d17ea5ef5
Alessandro Conceição Santos <alessanbaixista@gmail.com>
2024-04-11 09:53:02 -0300

    Configuração do banco de dados

--- Backend/gestao/src/app.module.ts
+++ Backend/gestao/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PessoasModule } from './pessoas/pessoas.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [PessoasModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
